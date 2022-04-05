5392d365f2cd200a4483b650770c6ff2b94064a5
TULI-MU <pauljoyosree12@gmail.com>
2022-04-05 18:43:09 +0600

    about page created

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About My<h2 className='trap'>Sound Trap</h2> Website</h1>
+        </div>
+    );
+};
+
+export default About;
