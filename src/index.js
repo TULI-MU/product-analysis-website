0400839e103cf86c63caa4d773ba26ba79ee4ab1
TULI-MU <pauljoyosree12@gmail.com>
2022-04-05 20:05:57 +0600

    redirects file added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOMClient from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const root = ReactDOMClient.createRoot(document.getElementById('root'));
+root.render(
   <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
   </BrowserRouter>,
-  document.getElementById('root')
+
 );
 
 // If you want to start measuring performance in your app, pass a function
